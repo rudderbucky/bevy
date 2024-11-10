de3e53c741c148ff7c3e7369e0f2e1e2935b6c40
rudderbucky <38639307+rudderbucky@users.noreply.github.com>
2024-11-09 22:52:31 -0800

    test

--- examples/dev_tools/fps_overlay_stress.rs
+++ examples/dev_tools/fps_overlay_stress.rs
@@ -6,6 +6,15 @@ use bevy::{
     text::FontSmoothing,
 };
 
+use bevy::{
+    reflect::TypePath,
+    render::render_resource::{AsBindGroup, ShaderRef},
+    sprite::{Material2d, Material2dPlugin},
+};
+
+/// This example uses a shader source file from the assets subdirectory
+const SHADER_ASSET_PATH: &str = "shaders/custom_material_2d_stress_test.wgsl";
+
 struct OverlayColor;
 
 impl OverlayColor {
@@ -17,6 +26,7 @@ fn main() {
     App::new()
         .add_plugins((
             DefaultPlugins,
+            Material2dPlugin::<CustomMaterial>::default(),
             FpsOverlayPlugin {
                 config: FpsOverlayConfig {
                     text_config: TextFont {
@@ -38,7 +48,12 @@ fn main() {
         .run();
 }
 
-fn setup(mut commands: Commands) {
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<CustomMaterial>>,
+    asset_server: Res<AssetServer>,
+) {
     // We need to spawn a camera (2d or 3d) to see the overlay
     commands.spawn(Camera2d);
 
@@ -57,6 +72,16 @@ fn setup(mut commands: Commands) {
             left: Val::Px(12.),
             ..default()
         },
+    ));
+
+    // quad
+    commands.spawn((
+        Mesh2d(meshes.add(Rectangle::new(2500., 2500.))),
+        MeshMaterial2d(materials.add(CustomMaterial {
+            color: LinearRgba::BLUE,
+            color_texture: Some(asset_server.load("branding/icon.png")),
+        })),
+        Transform::default(),
     ));
 }
 
@@ -77,5 +102,23 @@ fn customize_config(input: Res<ButtonInput<KeyCode>>, mut overlay: ResMut<FpsOve
     }
     if input.just_pressed(KeyCode::Digit4) {
         overlay.enabled = !overlay.enabled;
+    }
+}
+
+// This is the struct that will be passed to your shader
+#[derive(Asset, TypePath, AsBindGroup, Debug, Clone)]
+struct CustomMaterial {
+    #[uniform(0)]
+    color: LinearRgba,
+    #[texture(1)]
+    #[sampler(2)]
+    color_texture: Option<Handle<Image>>,
+}
+
+/// The Material2d trait is very configurable, but comes with sensible defaults for all methods.
+/// You only need to implement functions for features that need non-default behavior. See the Material2d api docs for details!
+impl Material2d for CustomMaterial {
+    fn fragment_shader() -> ShaderRef {
+        SHADER_ASSET_PATH.into()
     }
 }
